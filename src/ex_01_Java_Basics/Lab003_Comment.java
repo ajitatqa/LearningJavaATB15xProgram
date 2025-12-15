202137260afb52ab7910c71f38dd4923c87931f6
Ajit Kumar <ajitatqa@gmail.com>
2025-12-15 23:38:17 +0530

    submit

---
+++ src/ex_01_Java_Basics/Lab003_Comment.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Lab003_Comment {
+
+    public static void main(String[] args){
+
+        System.out.println("Welcome to India");
+        //System.out.println("Welcome to Delhi");
+    }
+}
---
+++ src/ex_01_Java_Basics/Test.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Test {
+
+    static void main(String[] args) {
+
+
+
+    }
+}
