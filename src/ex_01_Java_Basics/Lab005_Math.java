13907e282f21ddf4d5360ec07ae9e26da8a466e4
Ajit Kumar <ajitatqa@gmail.com>
2025-12-18 16:51:47 +0530

    math submit

---
+++ src/ex_01_Java_Basics/Lab005_Math.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab005_Math {
+    public static void main(String[] args){
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+}
+
