f644a6b73328dbede65dc2080f23b4278db1e7de
Ajit Kumar <ajitatqa@gmail.com>
2025-12-18 16:39:02 +0530

    submit

---
+++ src/ex_01_Java_Basics/Lab004_MultiComment.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab004_MultiComment {
+
+    public static void main (String[] args){
+
+       /** System.out.println("India");
+        System.out.println("Patna");
+        System.out.println("Delhi");**/
+        System.out.println("Noida");
+
+    }
+}
